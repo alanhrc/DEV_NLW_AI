e50c271ec7815f7475fce6a8ba515104d6b6fd52
Alan Henrique <alancamargo50@gmail.com>
2023-09-13 20:16:22 -0300

    feat: add `eslint`, `prettier` and `editorconfig` on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,151 @@
-import { Button } from './components/ui/button'
-import { Separator } from './components/ui/separator'
-import { FileVideo, Github, Upload, Wand2 } from 'lucide-react'
-import { Textarea } from './components/ui/textarea'
-import { Label } from './components/ui/label'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select'
-import { Slider } from './components/ui/slider'
+import { Button } from './components/ui/button';
+import { Separator } from './components/ui/separator';
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react';
+import { Textarea } from './components/ui/textarea';
+import { Label } from './components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select';
+import { Slider } from './components/ui/slider';
 
 export function App() {
   return (
-    <div className='min-h-screen flex flex-col'>
-      <div className='px-6 py-3 flex items-center justify-between border-b'>
-        <h1 className='text-xl font-bold'>upload.ai</h1>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
-        <div className='flex items-center gap-3'>
-          <span className='text-sm text-muted-foreground'>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
             Desenvolvido com 💚 no NLW da Rocketseat
           </span>
 
-          <Separator orientation='vertical' className='h-6' />
+          <Separator orientation="vertical" className="h-6" />
 
-          <Button variant='outline'>
-            <Github className='w-4 h-4 mr-2' />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
             Github
           </Button>
         </div>
       </div>
 
-      <main className='flex-1 p-6 flex gap-6'>
-        <div className='flex flex-col flex-1 gap-4'>
-          <div className='grid grid-rows-2 gap-4 flex-1'>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
             <Textarea
-              className='resize-none p-4 leading-relaxed'
-              placeholder='Inclua o prompt para a IA...' 
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
             />
-            <Textarea 
-              className='resize-none p-4 leading-relaxed'
-              placeholder='Resultado gerado pela IA...'
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
               readOnly
             />
           </div>
 
-          <p className='text-sm text-muted-foreground'>
-            Lembre-se: você pode utilizar a variável <code className='text-green-400'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do video selecionado.
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{' '}
+            <code className="text-green-400">{'{transcription}'}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do video
+            selecionado.
           </p>
         </div>
 
-        <aside className='w-80 space-y-4'>
-          <form className='space-y-4'>
-            <label 
+        <aside className="w-80 space-y-4">
+          <form className="space-y-4">
+            <label
               htmlFor="video"
-              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10'
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/10"
             >
-              <FileVideo className='w-6 h-6' />
+              <FileVideo className="w-6 h-6" />
               Selecione um vídeo
             </label>
 
-            <input type="file" id='video' accept='video/mp4' className='sr-only' />
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
 
             <Separator />
 
-            <div className='space-y-2'>
-              <Label htmlFor='transcription_prompt'>Prompt de transição</Label>
-              <Textarea className='h-20 leading-relaxed resize-none' id='transcription_prompt' placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)' />
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">Prompt de transição</Label>
+              <Textarea
+                className="h-20 leading-relaxed resize-none"
+                id="transcription_prompt"
+                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+              />
             </div>
 
-            <Button type='submit' className='w-full'>
+            <Button type="submit" className="w-full">
               Carregar vídeo
-              <Upload className='w-4 h-4 ml-2' />
+              <Upload className="w-4 h-4 ml-2" />
             </Button>
           </form>
 
           <Separator />
 
           <form className="space-y-2">
-            <div className='space-y-2'>
+            <div className="space-y-2">
               <Label>Prompt</Label>
               <Select>
                 <SelectTrigger>
-                  <SelectValue placeholder='Selecione um prompt...' />
+                  <SelectValue placeholder="Selecione um prompt..." />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value='title'>Título do YouTube</SelectItem>
-                  <SelectItem value='description'>Descrição do YouTube</SelectItem>
+                  <SelectItem value="title">Título do YouTube</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do YouTube
+                  </SelectItem>
                 </SelectContent>
               </Select>
             </div>
 
             <Separator />
 
-            <div className='space-y-2'>
+            <div className="space-y-2">
               <Label>Modelo</Label>
-              <Select defaultValue='gpt3.5' disabled>
+              <Select defaultValue="gpt3.5" disabled>
                 <SelectTrigger>
                   <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value='gpt3.5'>GPT 3.5-turbo 16K</SelectItem>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16K</SelectItem>
                 </SelectContent>
               </Select>
 
-              <span className='block text-xs text-muted-foreground italic'>Você poderá customizar essa opção em breve</span>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
             </div>
 
             <Separator />
 
-            <div className='space-y-4'>
+            <div className="space-y-4">
               <Label>Temperatura</Label>
 
               <Slider min={0} max={1} step={0.1} />
 
-              <span className='block text-xs text-muted-foreground italic leading-relaxed'>Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros</span>
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros
+              </span>
             </div>
 
             <Separator />
 
-            <Button type='submit' className='w-full'>
+            <Button type="submit" className="w-full">
               Executar
-              <Wand2 className='w-4 h-4 ml-2' />
+              <Wand2 className="w-4 h-4 ml-2" />
             </Button>
           </form>
         </aside>
       </main>
     </div>
-  )
+  );
 }
